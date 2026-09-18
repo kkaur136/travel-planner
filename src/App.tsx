33169e1a3a173853057f1007ca789eab05f3e599
Gurmandeep Kaur <gkaur121@rrc.ca>
2026-09-17 23:49:07 -0500

    making changes in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Destinations from "./components/destinations/destinations";
  
 function App() {
   const [count, setCount] = useState(0)
@@ -114,6 +115,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
+      <Destinations />
       <section id="spacer"></section>
     </>
   )
