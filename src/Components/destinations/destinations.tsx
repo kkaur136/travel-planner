5d363a7c60d282daf5ed2f65fbc0c9ccbf951bdb
Gurmandeep Kaur <gkaur121@rrc.ca>
2026-09-18 13:15:12 -0500

    Update destinations trip data

--- src/Components/destinations/destinations.tsx
+++ src/Components/destinations/destinations.tsx
@@ -4,30 +4,30 @@ function Destinations() {
     const destinations = [
         {
             id: 1,
-            city: "Toronto",
-            country: "Canada",
+            city: "Paris",
+            country: "France",
         },
         {
             id: 2,
-            city: "New York",
-            country: "USA",
+            city: "Nice",
+            country: "France",
         },
         {
             id: 3,
-            city: "London",
-            country: "UK",
+            city: "Lyon",
+            country: "France",
         },
         {
             id: 4,
-            city: "Paris",
+            city: "Bordeaux",
             country: "France",
         }
     ];
 
     return (
         <section className="destinations">
-            <h2>Destinations</h2>
-            <p>Explore destinations for your next trip.</p>
+            <h2>Destinations for My France Trip</h2>
+            <p>Destinations planned for my France trip.</p>
 
             <ul>
                 {destinations.map((destination) => (
