352c964a7043230d943905e52537d30d8ed44e16
khushpreet kaur <khush08022006preet@gmail.com>
2026-09-18 15:05:44 -0500

    Fix import paths to use correct casing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Destinations from "./components/destinations/destinations";
-import Activities from "./components/activities/Activities";
-import SavedDestinations from "./components/saved-destinations/SavedDestinations";
+import Destinations from "./Components/destinations/destinations";
+import Activities from "./Components/Activities/Activities";
+import SavedDestinations from "./Components/saved-destinations/SavedDestinations";
 
 function App() {
   return (
